6215c2c5887967d7e51bd2a3d6fc0d9d2903ea8e
Max Bossing <info@maxbossing.de>
2025-07-16 11:46:49 +0200

    chore: add explanation and additional info to cli

--- src/cli.rs
+++ src/cli.rs
@@ -2,8 +2,9 @@ use std::path::PathBuf;
 use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
+#[clap(author = "Max Bossing", version, about = "System-agnostic dotfile deployer", long_about = None)]
 pub struct Cli {
-    #[arg(default_value = "dots.toml")]
+    #[arg(short, long, default_value = "dots.toml")]
     pub config: Option<PathBuf>,
     
     #[command(subcommand)]
@@ -12,6 +13,8 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum CliCommand {
+    #[clap(about = "Deploys a dots set")]
     Deploy,
+    #[clap(about = "Unlinks (tries to remove) a dots deployment")]
     Unlink
 }
